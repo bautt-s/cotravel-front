12fa9463fcf563fcb3da074eac14a41829e9d90d
bautt-s <bauti_hora@hotmail.com>
2023-06-23 01:55:37 -0300

    feat: made header and footer responsive

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -18,30 +18,36 @@ const FooterSection: React.FC = () => {
 
     return (
         <div className="bg-[#232528] text-white flex flex-col pt-[40px]">
-            <div className="grid grid-cols-7 pb-[80px] px-[120px]">
+            <h2 className="relative before:absolute before:bg-[#f15221] before:bottom-0 before:left-0 before:h-full w-fit flex xl:hidden
+                        before:w-full before:origin-bottom before:scale-y-[0.3] hover:before:scale-y-100 before:transition-transform mx-auto
+                        before:ease-in-out before:duration-500 font-semibold text-3xl cursor-pointer h-fit merriweather">
+                <span className="relative">CoTravel</span>
+            </h2>
 
-                <h2 className="relative before:absolute before:bg-[#f15221] before:bottom-0 before:left-0 before:h-full w-fit
+            <div className="grid grid-cols-2 lg:grid-cols-6 xl:grid-cols-7 pb-[80px] mx-[40px] lg:mx-[60px] xl:mx-[120px] lg:mt-[40px] xl:mt-0">
+                <h2 className="relative before:absolute before:bg-[#f15221] before:bottom-0 before:left-0 before:h-full w-fit hidden xl:flex
                         before:w-full before:origin-bottom before:scale-y-[0.3] hover:before:scale-y-100 before:transition-transform 
                         before:ease-in-out before:duration-500 font-semibold text-3xl cursor-pointer h-fit merriweather">
                     <span className="relative">CoTravel</span>
                 </h2>
 
                 {sections.map((s, index) =>
-                    <div key={index}>
-                        <h3 className="text-[#c4c4c4] font-bold">{s.title}</h3>
+                    <div key={index} className='mt-[80px] lg:mt-0'>
+                        <h3 className="text-[#c4c4c4] font-bold lg:text-sm xl:text-base">{s.title}</h3>
 
                         <ul className="mt-[30px] font-semibold">
                             {s.items.map((i, indexItem) =>
-                                <li key={indexItem} className="mt-[20px] text-sm hover:text-[#f15221] transition-colors duration-300 w-fit h-fit cursor-pointer">{i}</li>
+                                <li key={indexItem} className="mt-[20px] text-sm hover:text-[#f15221]  
+                                transition-colors duration-300 w-fit h-fit cursor-pointer">{i}</li>
                             )}
                         </ul>
                     </div>)}
 
-                <div>
+                <div className='mt-[80px] lg:mt-0 xl:w-fit'>
                     <h3 className="text-[#b8b8b8] font-bold">FOLLOW US</h3>
 
                     <div className="flex flex-col mt-[30px]">
-                        <div className="flex flex-row text-white gap-x-[25px]">
+                        <div className="flex flex-row text-white gap-x-[25px] lg:gap-x-[15px] xl:gap-x-[25px]">
                             <BsTwitter className='text-2xl cursor-pointer hover:text-[#f15221] transition-colors duration-300' />
                             <ImFacebook2 className='text-2xl cursor-pointer hover:text-[#f15221] transition-colors duration-300' />
                             <BsInstagram className='text-2xl cursor-pointer hover:text-[#f15221] transition-colors duration-300' />
@@ -68,15 +74,17 @@ const FooterSection: React.FC = () => {
                 </div>
             </div>
 
-            <div className='bg-[#131313] py-[15px] flex flex-row px-[120px]'>
-                <span className='text-sm text-[#a1a1a1] font-semibold'>© Copyright - CoTravel 2024</span>
+            <div className='bg-[#131313] py-[15px] flex lg:flex-row flex-col px-0 lg:px-[60px] xl:px-[120px]'>
+                <span className='text-sm text-[#a1a1a1] font-semibold hidden lg:flex'>© Copyright - CoTravel 2023</span>
 
-                <ul className='ml-auto text-sm flex flex-row gap-[80px] font-semibold text-[#e4e4e4]'>
+                <ul className='mx-auto lg:mx-0 lg:ml-auto text-xs lg:text-sm flex flex-row gap-[20px] lg:gap-[80px] font-semibold text-[#e4e4e4]'>
                     <li className='cursor-pointer'>Cookie Notice</li>
                     <li className='cursor-pointer'>Privacy Policy</li>
                     <li className='cursor-pointer'>Terms & Conditions</li>
                     <li className='cursor-pointer'>Security & Compliance</li>
                 </ul>
+
+                <span className='text-sm text-[#a1a1a1] font-semibold flex lg:hidden mx-auto mt-[25px]'>© Copyright - CoTravel 2023</span>
             </div>
         </div>
     )
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,10 @@
 import { MdTravelExplore } from 'react-icons/md'
+import { HiMenu } from 'react-icons/hi'
 
 const HeaderLandscape: React.FC = () => {
     return (
         <div className="w-full h-screen header-bg">
-            <nav className="flex flex-row text-white py-[60px] px-[60px] items-center">
+            <nav className="flex flex-row text-white py-[60px] px-[60px] md:px-[30px] lg:px-[60px] items-center">
                 <div className='flex flex-row items-center group hover:scale-105 transition-transform duration-300 z-50'>
                     <MdTravelExplore className='text-4xl text-[#f15221]' />
                     <span className="font-semibold text-2xl cursor-pointer ml-[10px] mb-[3px]">
@@ -11,7 +12,7 @@ const HeaderLandscape: React.FC = () => {
                     </span>
                 </div>
 
-                <ul className="flex flex-row gap-[60px] ml-auto font-semibold text-lg">
+                <ul className="hidden flex-row md:gap-[20px] lg:gap-[40px] xl:gap-[60px] ml-auto font-semibold lg:text-lg md:flex">
                     <li className="cursor-pointer relative after:absolute after:bg-gray-200 after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 hover:after:origin-bottom-left hover:after:scale-x-100 after:transition-transform after:ease-in-out after:duration-300">Destinations</li>
                     <li className="cursor-pointer relative after:absolute after:bg-gray-200 after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 hover:after:origin-bottom-left hover:after:scale-x-100 after:transition-transform after:ease-in-out after:duration-300">About us</li>
                     <li className="cursor-pointer relative after:absolute after:bg-gray-200 after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 hover:after:origin-bottom-left hover:after:scale-x-100 after:transition-transform after:ease-in-out after:duration-300">Gallery</li>
@@ -22,12 +23,16 @@ const HeaderLandscape: React.FC = () => {
                         <span className="ml-[5px] inline-block translate-x-0 group-hover:translate-x-1 transition-transform ease-in-out duration-200">→</span>
                     </li>
                 </ul>
+
+                <HiMenu className='flex md:hidden ml-auto text-4xl hover:text-[#f15221] z-50 transition-all duration-300' />
             </nav>
 
-            <div className="flex flex-col text-white ml-[80px] mt-[100px]">
-                <h1 className="text-6xl font-semibold w-[640px] merriweather leading-[75px] z-50">Explore the world with exciting people</h1>
+            <div className="flex flex-col text-white md:ml-[80px] mt-[120px] xl:mt-[100px] text-center md:text-left items-center md:items-start">
+                <h1 className="text-4xl md:text-6xl font-semibold max-w-[17ch] md:w-[640px] merriweather leading-[75px] z-50">
+                    Explore the world with exciting people
+                </h1>
 
-                <span className="mt-[40px] text-lg font-semibold z-50">We help people find ideal joint trip company</span>
+                <span className="mt-[20px] md:mt-[40px] text-lg md:font-semibold z-50">We help people find ideal joint trip company</span>
 
                 <div className="group w-[200px] [transform:translateZ(0)] px-6 py-3 rounded-full bg-[#f15221] overflow-hidden mt-[40px] text-center
                 relative before:absolute before:bg-[#fd7850] before:bottom-0 before:left-0 before:h-full before:w-full before:duration-500 cursor-pointer
@@ -36,7 +41,7 @@ const HeaderLandscape: React.FC = () => {
                 </div>
             </div>
 
-            <div className="flex flex-row text-white px-[120px] gap-x-auto mt-[180px]">
+            <div className="hidden flex-row text-white lg:px-[80px] xl:px-[120px] gap-x-auto lg:mt-[140px] xl:mt-[180px] lg:flex">
                 <div className="flex flex-col gap-y-[10px] z-50">
                     <span className="text-3xl font-semibold merriweather">01</span>
                     <h3 className="text-xl font-semibold">Choose place, activity and time</h3>
@@ -49,7 +54,7 @@ const HeaderLandscape: React.FC = () => {
                     <p className="text-lg w-[380px]">You can find people with the same interest, who know the routes you've never travelled.</p>
                 </div>
 
-                <div className="flex flex-col gap-y-[10px] ml-auto z-50">
+                <div className="hidden xl:flex flex-col gap-y-[10px] ml-auto z-50">
                     <span className="text-3xl font-semibold merriweather">03</span>
                     <h3 className="text-xl font-semibold">Start chat and offer to go on a joint adventure</h3>
                     <p className="text-lg w-[400px]">Discuss the details of your trip and have a great time! You might find a great friend for life.</p>
